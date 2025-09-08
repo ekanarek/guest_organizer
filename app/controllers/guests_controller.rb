4e0ab1f9474b9a85f39c6ffcf0885a3f7555e75e
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-08 12:20:39 -0400

    Require login before running other actions

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,9 @@
+class GuestsController < ApplicationController 
+  before_action :require_login 
+
+  private 
+
+  def require_login 
+    return head(:forbidden) unless session.include? :user_id 
+  end
+end
---
+++ app/controllers/tables_controller.rb
@@ -0,0 +1,9 @@
+class TablesController < ApplicationController 
+  before_action :require_login 
+
+  private 
+
+  def require_login 
+    return head(:forbidden) unless session.include? :user_id 
+  end
+end
