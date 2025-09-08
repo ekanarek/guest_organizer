fbf29f35e636bc8898f1d617181d218cfae2cf7a
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-08 12:37:42 -0400

    Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_08_161611) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_08_163712) do
   create_table "dietary_restrictions", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -37,6 +37,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_08_161611) do
     t.string "name"
     t.integer "seat_capacity"
     t.integer "seats_taken"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
