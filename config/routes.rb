462714a9843a0fb78782c625b47b5ace207654c7
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-09 11:48:41 -0400

    Edit and delete routes for guests

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new" 
   post "/login", to: "sessions#create"
 
-  resources :guests, only: [:new, :create, :show]
+  resources :guests, only: [:new, :create, :show, :edit, :update, :destroy]
 end
