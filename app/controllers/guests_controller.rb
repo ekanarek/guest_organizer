032ac29d75d6281776e04020cd4bfc14861076d3
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-09 09:07:35 -0400

    Fix typo

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -7,7 +7,7 @@ class GuestsController < ApplicationController
 
   def new 
     @guest = Guest.new(user: current_user)
-    @dietary_restriction = DietaryRestriction.where(user_id: [nil, current_user.id])
+    @dietary_restrictions = DietaryRestriction.where(user_id: [nil, current_user.id])
   end
 
   def create 
@@ -26,7 +26,7 @@ class GuestsController < ApplicationController
         @guest.table.increment!(:seats_taken) 
       end
 
-      redirect_to guests_path, notice: "Guest added!" 
+      redirect_to root_path, notice: "Guest added!" 
     else 
       flash.now[:alert] = "Error creating guest" 
       render :new, status: :unprocessable_entity 
@@ -36,6 +36,6 @@ class GuestsController < ApplicationController
   private 
 
   def guest_params 
-    params.require(:guest).permit(:name, :age, :table_id, :dietary_restriction_ids: []) 
+    params.require(:guest).permit(:name, :age, :table_id, dietary_restriction_ids: []) 
   end
 end
