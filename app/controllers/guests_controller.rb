f4d082e341fdd1f7141331f1816fd1256a499a1e
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-09 08:59:00 -0400

    Create a new guest

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -7,8 +7,35 @@ class GuestsController < ApplicationController
 
   def new 
     @guest = Guest.new(user: current_user)
+    @dietary_restriction = DietaryRestriction.where(user_id: [nil, current_user.id])
   end
 
   def create 
+    @guest = current_user.guests.build(guest_params)
+
+    if params[:new_dietary_restriction_name].present? 
+      custom_restriction = current_user.dietary_restrictions.create(
+        name: params[:new_dietary_restriction_name],
+        description: params[:new_dietary_restriction_description]
+      )
+      @guest.dietary_restrictions << custom_restriction if custom_restriction.persisted? 
+    end
+
+    if @guest.save 
+      if @guest.table.present? 
+        @guest.table.increment!(:seats_taken) 
+      end
+
+      redirect_to guests_path, notice: "Guest added!" 
+    else 
+      flash.now[:alert] = "Error creating guest" 
+      render :new, status: :unprocessable_entity 
+    end
+  end
+
+  private 
+
+  def guest_params 
+    params.require(:guest).permit(:name, :age, :table_id, :dietary_restriction_ids: []) 
   end
 end
